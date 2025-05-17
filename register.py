a366b7035e72430934467339660a62b9aabdf2f0
Santiago Uribe <santiuribeduarte@gmail.com>
2025-05-17 17:43:30 -0500

    :Feat: Fixing the dicctionariys :joy_cat:

--- register.py
+++ register.py
@@ -8,3 +8,39 @@ def guardar_data(listar_datos):
     with open (file,"w") as file:
         return json.dump(listar_datos,file,indent=4)
 
+def registrar_usuario():
+    datos = leer_data()
+
+    if "usuarios" not in datos:
+        datos["usuarios"] = []
+
+    email = input("Ingrese su email: ")
+    if usuario_existente(email):
+        print("⚠️ El usuario ya existe.")
+        return None
+
+    nombre = input("Ingrese su nombre: ")
+    apellido = input("Ingrese su apellido: ")
+    password = input("Ingrese su contraseña: ")
+
+    nuevo_usuario = {
+        "nombre": nombre,
+        "apellido": apellido,
+        "email": email,
+        "password": password,
+        "gastos": []
+    }
+
+    datos["usuarios"].append(nuevo_usuario)
+    guardar_data(datos)
+    print("✅ Usuario registrado con éxito.")
+    return nuevo_usuario
+
+def usuario_existente(email):
+    datos = leer_data()
+    if "usuarios" in datos:
+        for usuario in datos["usuarios"]:
+            if usuario["email"] == email:
+                print("Bienvenido de nuevo")
+            elif usuario["email"] != email:
+                print("Usuario no encontrado")
